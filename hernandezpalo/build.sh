438a394526e9738a0feddf41a2564a90106215f8
Javier Hernandez <=>
2024-04-11 19:35:27 +0200

    despliegue front

--- hernandezpalo/build.sh
+++ hernandezpalo/build.sh
@@ -8,6 +8,7 @@ cd hernandezpalo
 pip install --upgrade pip
 pip install -r requirements.txt
 rm -fr public
+cd ..
 reflex init
 API_URL="https://hernandezpalo.onrender.com" reflex export --frontend-only
 unzip frontend.zip -d public
