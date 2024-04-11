1a0a21ba57b0d66772a1dc9666d9232dbbbcca49
Javier Hernandez <=>
2024-04-11 20:01:09 +0200

    action github

---
+++ hernandezpalo/remote_build.sh
@@ -0,0 +1,18 @@
+
+python -m venv .venv
+cd ..
+cd .venv
+cd bin
+source activate
+cd ..
+cd ..
+cd hernandezpalo
+pip install --upgrade pip
+pip install -r requirements.txt
+rm -fr public
+cd ..
+reflex init
+API_URL="https://hernandezpalo.onrender.com" reflex export --frontend-only
+unzip frontend.zip -d public
+rm -f frontend.zip
+deactivate
