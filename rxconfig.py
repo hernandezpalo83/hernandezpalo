8cb2610e8e3c44950047ac36e099d99a72e5375e
Javier Hernandez <=>
2024-04-11 19:41:14 +0200

    .

--- rxconfig.py
+++ rxconfig.py
@@ -3,6 +3,7 @@ import reflex as rx
 config = rx.Config(
     app_name="hernandezpalo",
 )
+
     # cors_allowed_origins=[
     #         'https://rxh-prod-hernandezpalo.fly.dev',
     #         'http://localhost:3000',
