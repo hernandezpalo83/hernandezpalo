91fee2fdf217d8d84ceaffd590f610b42a2522ea
Javier Hernandez <=>
2024-04-26 15:14:11 +0200

    .

--- hernandezpalo/remote_build.sh
+++ hernandezpalo/remote_build.sh
@@ -2,7 +2,6 @@ python -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
-rm -fr public
 reflex init
 API_URL="https://hernandezpalo.onrender.com" reflex export --frontend-only
 unzip frontend.zip -d public
