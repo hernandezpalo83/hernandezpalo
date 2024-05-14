aeb27854e8d889812ca3f9bbd480a5e8b6613072
Javier Hernandez <=>
2024-05-14 20:22:19 +0200

    Prueba para ver si compila

--- hernandezpalo/constants.py
+++ hernandezpalo/constants.py
@@ -3,6 +3,7 @@ GITHUB_URL = "https://github.com/hernandezpalo83"
 TWITTER_X_URL = "https://twitter.com/JHernandez14153"
 LINKEDIN_URL = "https://www.linkedin.com/in/javier-hernandez-martin-6473b321/"
 
+
 # Comunidad
 CODE_CHALLENGES_URL = "https://retosdeprogramacion.com"
 
