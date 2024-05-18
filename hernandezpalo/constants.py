10013d3f64ffe68f4017b7736eec21be66a48ced
Javier Hernandez <=>
2024-05-18 17:32:28 +0200

    cambios url poniendo el dominio

--- hernandezpalo/constants.py
+++ hernandezpalo/constants.py
@@ -12,7 +12,7 @@ COFFEE_URL = "https://buymeacoffee.com/mouredev"
 
 # Contacto
 EMAIL = "hernandezpao@gmail.com"
-HERNANDEZPALO_URL = "https://portafolio.reflex.run"  #"https://hernandezpalo.dev"
+HERNANDEZPALO_URL = "https://javihernandez.dev"  #"https://hernandezpalo.dev"
 
 #CERTIFICACIONES
 CERT_PYTHON_ESSENTIAL_1 = "https://www.credly.com/badges/8e5b7cff-ec6f-4b8e-82d0-dcbd6307ec21/linked_in_profile"
@@ -23,9 +23,9 @@ CERT_Junior_cyber="https://www.credly.com/badges/a5bdebca-cec7-456f-a079-65f4c68
 CERT_LINKELID="https://www.linkedin.com/learning/certificates/aedef02f298d7f56c6cef928fafa3b845b8e2b497cee482dfb47aa7f7e49c6de"
 
 #Proyectos
-PROY_RESTAURANTE= "https://reflex-restaurante.vercel.app/"
-PROY_BLOG="https://hernandezpalo.pythonanywhere.com"
+PROY_RESTAURANTE= "https://restaurante.javihernandez.dev"
+PROY_BLOG="https://blog.javihernandez.dev"
 PROY_LOTERIA = "https://loteria-navidad.reflex.run"
-PROY_POKEDEX = "https://reflex-pokedex.reflex.run"
+PROY_POKEDEX = "https://pokédex.javihernandez.dev"
 
 GOOGLE_ANALYTICS_TAG = "G-DNWQHT98K2"
