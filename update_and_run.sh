538e4b262bb9db2ff576f61e80894892e3df7c87
Hernandezpalo <hernandezpalo@gmail.com>
2024-05-21 19:54:12 +0200

    Update update_and_run.sh

--- update_and_run.sh
+++ update_and_run.sh
@@ -10,12 +10,12 @@ git clone https://github.com/hernandezpalo83/hernandezpalo.git
 sudo rm Dockerfile
 mv Dockerfile_Local Dockerfile
 
-# Construir la imagen de Docker
-sudo docker build -t portafolio .
-
 # Detener y eliminar el contenedor antiguo (si existe)
 sudo docker stop portafolio
 sudo docker rm portafolio
+
+# Construir la imagen de Docker
+sudo docker build -t portafolio:latest .
 
 # Levantar un nuevo contenedor
 docker run -d -p 3000:3000 -p 8000:8000 --name app portafolio:latest
